ce850db7b14cae0ff423002fc09fe08c63ef6c07
Ruslan <ruslan15-mol@mail.ru>
2025-02-06 12:56:50 +0300

    model.py -> comment changes

--- swin_transformer/model.py
+++ swin_transformer/model.py
@@ -16,11 +16,11 @@ def window_partition(x, window_size=4):
     Devides patched tensor into windows.
 
     Parameters:
-        x (tensor): tensor of patches -> (B, H, W, C)
+        x (torch.Tensor): tensor of patches -> (B, H, W, C)
         window_size (int): size of window
 
     Returns:
-        tensor: tensor of windows -> (n * B, window_size, window_size, C)
+        torch.Tensor: tensor of windows -> (n * B, window_size, window_size, C)
     """
 
     assert len(x.shape) == 4, "suppports only batched tensors"
@@ -36,11 +36,11 @@ def window_merging(x, window_size, h, w):
     Assembles tensor of windows into single patched tensor.
 
     Parameters:
-        x (tensor): tensor of windows -> (n * B, window_size, window_size, C)
+        x (torch.Tensor): tensor of windows -> (n * B, window_size, window_size, C)
         window_size (int): size of window
 
     Returns:
-        tensor: tensor of patches -> (B, H, W, C)
+        torch.Tensor: tensor of patches -> (B, H, W, C)
     """
 
     assert len(x.shape) == 4, "suppports only batched tensors"
@@ -82,18 +82,18 @@ def build_mask(resolution, winsize, shift):
     return attn_mask
 
 
-# --------------------------------------------- Computational blocks (small ones) -------------------------------------------------- #
+# ----------------------------------------------- Basic Computational Blocks ---------------------------------------------------- #
 
 
 class PatchEmbedding(nn.Module):
     """
-    Splits input image into patches -> maps patches to embedding vectors
+    Splits input image into patches -> maps patches to embedding vectors.
 
     Parameters:
         img_size (int): size of input image (supposed it comes in a square form)
         patch_size (int): length of patch's side in pixels
         emb_dim (int): dimension of aquired patch embeddings
-        norm_layer (nn.Module / None): normalization to apply afterwards
+        norm_layer (nn.Module, optional): normalization to apply afterwards
     """
 
     def __init__(self, img_size=224, patch_size=4, emb_dim=16, norm_layer=None):
@@ -111,12 +111,12 @@ class PatchEmbedding(nn.Module):
 
     def forward(self, x):
         assert len(x.shape) == 4, "only batched 3d tensors supported"
-        assert x.shape[3] == 3, "input tensor has to be in (H, W, 3) format"
+        assert x.shape[3] == 3, "input tensor has to be in (B, H, W, 3) format"
         assert x.shape[1] % self.patch_size == 0, "tensor size has to be divisible by patch_size"
 
-        permuted_x = x.permute(0, 3, 1, 2)      # to (bchw)
+        permuted_x = x.permute(0, 3, 1, 2)      # -> (B, C, H, W)
         res = self.patch_conv(permuted_x)
-        res = res.permute(0, 2, 3, 1)           # back to (bhwc)
+        res = res.permute(0, 2, 3, 1)           # -> (B, H, W, C)
         if self.norm_layer is not None:
             res = self.norm_layer(res)
 
@@ -125,7 +125,7 @@ class PatchEmbedding(nn.Module):
 
 class MLP(nn.Module):
     """
-    Multi-Layer Perceptron
+    Multi-Layer Perceptron.
 
     Parameters:
         in_features (int): number of input tensor's features
@@ -158,16 +158,16 @@ class MLP(nn.Module):
 
 class WMSA(nn.Module):
     """
-    Window-based Multi-head Self Attention
+    Window-based Multi-head Self Attention.
 
     Parameters:
-        input _dim (int): Number of input channels.
-        window_size (tuple[int]): The height and width of the window.
-        n_heads (int): Number of attention heads.
-        qkv_bias (bool, optional):  If True, add a learnable bias to query, key, value. Default: True
-        qk_scale (float | None, optional): Override default qk scale of head_dim ** -0.5 if set
-        attn_drop (float, optional): Dropout ratio of attention weight. Default: 0.0
-        output_drop (float, optional): Dropout ratio of output. Default: 0.0
+        input _dim (int): number of input channels
+        window_size (tuple[int]): the height and width of the window
+        n_heads (int): Number of attention heads
+        qkv_bias (bool, optional):  if True, add a learnable bias to query, key, value
+        qk_scale (float, optional): override default qk scale of head_dim ** -0.5 if set
+        attn_drop (float, optional): dropout ratio of attention weight
+        output_drop (float, optional): dropout ratio of output
     """
 
     def __init__(self, input_dim, window_size, n_heads,
@@ -187,7 +187,7 @@ class WMSA(nn.Module):
 
         # define a parameter table of Relative Position Bias
         self.rpb_table = nn.Parameter(
-            torch.zeros((2 * window_size[0] - 1) * (2 * window_size[1] - 1), n_heads)   # (2*Wh-1 * 2*Ww-1, nH)
+            torch.zeros((2 * window_size[0] - 1) * (2 * window_size[1] - 1), n_heads)   # -> (2*Wh-1 * 2*Ww-1, nH)
         )
 
         # constructing Relative Position Bias index matrix
@@ -207,14 +207,14 @@ class WMSA(nn.Module):
 
     def forward(self, x, mask=None):
         x = x.permute(0, 3, 1, 2)               # -> (bW, C, Wh, Ww)
-        bW, c, Wh, Ww = x.shape                 # bW = b * nW
+        bW, c, Wh, Ww = x.shape                 # bW = B * nW
 
         qkv = self.to_qkv(x).chunk(3, dim=1)    # -> 3 x (bW, C, Wh, Ww)
 
         q, k, v = map(
             lambda t: rearrange(t, "b (h d) x y -> b h d (x y)", h=self.n_heads), qkv
         )
-        q = q * self.qk_scale    # -> (bW, nH, C, Wh*Ww)  for q, k, v
+        q = q * self.qk_scale                                # -> (bW, nH, C, Wh*Ww)  for q, k, v
 
         sim = einsum("b h d i, b h d j -> b h i j", q, k)    # -> (bW, nH, Wh*Ww, Wh*Ww)
         sim = sim - sim.amax(dim=-1, keepdim=True).detach()
@@ -222,16 +222,17 @@ class WMSA(nn.Module):
         rpb = self.rpb_table[self.relative_position_index.view(-1)].view(
             self.window_size[0] * self.window_size[1], self.window_size[0] * self.window_size[1], -1   # -> (Wh*Ww, Wh*Ww, nH)
         )
-
         rpb = rpb.permute(2, 0, 1).contiguous()           # -> (nH, Wh*Ww, Wh*Ww)
+
         sim = sim + rpb.unsqueeze(0)                      # rpb -> (1, nH, Wh*Ww, Wh*Ww)
         if mask is not None:
-            nW = mask.shape[0]                            # -> n of windows in single img
+            nW = mask.shape[0]                            # -> n of windows in single image
             sim = rearrange(sim, "(b nW) nH i j -> b nW nH i j", nW=nW)
             sim = sim + mask.unsqueeze(1).unsqueeze(0)    # mask -> (1, nW, 1, Wh*Ww, Wh*Ww)
-        attn = sim.softmax(dim=-1)
-        if mask is not None:
+            attn = sim.softmax(dim=-1)
             attn = rearrange(attn, "b nW nH i j -> (b nW) nH i j")
+        else:
+            attn = sim.softmax(dim=-1)
 
         output = einsum("b h i j, b h d j -> b h i d", attn, v)                 # -> (bW, nH, Wh*Ww, C)
         output = rearrange(output, "b h (x y) d -> b (h d) x y", x=Wh, y=Ww)    # -> (bW, C, Wh, Ww)
@@ -243,7 +244,7 @@ class WMSA(nn.Module):
 
 class PatchMerging(nn.Module):
     """
-    Patch Merging (downsampling method)
+    Patch Merging (downsampling method).
 
     Parameters:
         input_dim (int): number of channels in input tensor
@@ -256,7 +257,7 @@ class PatchMerging(nn.Module):
         self.reduction = nn.Conv2d(input_dim, 2 * input_dim, kernel_size=2, stride=2, bias=False)
         self.norm_layer = norm_layer(input_dim)
 
-    def forward(self, x):    # x -> (B, H, W, C)
+    def forward(self, x):           # x -> (B, H, W, C)
         x = self.norm_layer(x)
         x = x.permute(0, 3, 1, 2)   # -> (B, C, H, W)
         x = self.reduction(x)
@@ -265,26 +266,27 @@ class PatchMerging(nn.Module):
         return x
         
 
-# --------------------------------------------- Construction blocks (big ones) -------------------------------------------------- #
+# ----------------------------------------------- Architectural Blocks ---------------------------------------------------- #
 
 class SwinTransformerBlock(nn.Module):
     """
-    Swin Transformer Block
+    Swin Transformer Block.
+    (Applies normalization, WMSA and MLP)
 
     Parameters:
         input_dim (int): number of channels in input tensor
-        input_resolution (Tuple[int]): input resulotion
+        input_resolution (Tuple[int, int]): input resulotion
         n_heads (int): number of attentions heads
         window_size (int): window size
-        shift_size (int): shift size for SW-MSA.
+        shift_size (int): shift size for shifting in WMSA
         mlp_ratio (float): ratio of mlp hidden dim to embedding dim.
-        qkv_bias (bool, optional): If True, add a learnable bias to query, key, value. Default: True
-        qk_scale (float | None, optional): Override default qk scale of head_dim ** -0.5 if set.
-        mlp_drop (float, optional): Dropout rate. Default: 0.0
-        attn_drop (float, optional): Attention dropout rate. Default: 0.0
-        drop_path (float, optional): Stochastic depth rate. Default: 0.0
-        act_layer (nn.Module, optional): Activation layer. Default: nn.GELU
-        norm_layer (nn.Module, optional): Normalization layer.  Default: nn.LayerNorm
+        qkv_bias (bool, optional): if True, add a learnable bias to query, key, value
+        qk_scale (float, optional): Override default qk scale of head_dim ** -0.5 if set
+        mlp_drop (float, optional): dropout rate
+        attn_drop (float, optional): attention dropout rate
+        drop_path (float, optional): stochastic depth rate
+        act_layer (nn.Module, optional): activation layer
+        norm_layer (nn.Module, optional): normalization layer
     """
 
     def __init__(self, input_dim, input_resolution, n_heads,
@@ -293,7 +295,7 @@ class SwinTransformerBlock(nn.Module):
                  act_layer=nn.GELU, norm_layer=nn.LayerNorm):
         super().__init__()
         self.input_dim = input_dim
-        self.input_resolution = input_resolution    # нужно передать, чтобы создать маску без входной картинки
+        self.input_resolution = input_resolution 
         self.n_heads = n_heads
         self.window_size = window_size
         self.shift_size = shift_size
@@ -362,22 +364,22 @@ class SwinTransformerBlock(nn.Module):
 class BasicLayer(nn.Module):
     """
     Basic Swin Transformer layer.
-    Stacks several Swin Blocks and puts the downsampler afterwords.
+    (Stacks several Swin Blocks and puts the downsampler afterwords)
 
     Parameters:
-        input_dim (int): Number of input channels.
-        input_resolution (tuple[int]): Input resolution.
-        depth (int): Number of blocks.
-        n_heads (int): Number of attention heads.
-        window_size (int): Local window size.
-        mlp_ratio (float): Ratio of MLP hidden dim to embedding dim.
-        qkv_bias (bool, optional): If True, add a learnable bias to query, key, value. Default: True
-        qk_scale (float | None, optional): Override default qk scale of head_dim ** -0.5 if set.
-        mlp_drop (float, optional): MLP dropout rate. Default: 0.0
-        attn_drop (float, optional): Attention dropout rate. Default: 0.0
-        drop_path (float | tuple[float], optional): Stochastic depth rate. Default: 0.0
-        norm_layer (nn.Module, optional): Normalization layer. Default: nn.LayerNorm
-        downsample (nn.Module | None, optional): Downsample layer at the end of the layer. Default: None
+        input_dim (int): number of input channels
+        input_resolution (Tuple[int, int]): input resolution
+        depth (int): number of blocks
+        n_heads (int): number of attention heads
+        window_size (int): local window size
+        mlp_ratio (float): ratio of MLP hidden dim to embedding dim
+        qkv_bias (bool, optional): if True, add a learnable bias to query, key, value
+        qk_scale (float, optional): Override default qk scale of head_dim ** -0.5 if set
+        mlp_drop (float, optional): MLP dropout rate
+        attn_drop (float, optional): attention dropout rate
+        drop_path (float | Tuple[float], optional): stochastic depth rate
+        norm_layer (nn.Module, optional): normalization layer
+        downsample (nn.Module, optional): downsample layer at the end of the layer
     """
 
     def __init__(self, input_dim, input_resolution, depth, n_heads, window_size,
@@ -411,16 +413,17 @@ class BasicLayer(nn.Module):
 
     def forward(self, x):
         for block in self.blocks:
-            x = block(x)
+            x = block(x)                     # -> (B, H, W, C)
         if self.downsample is not None:
-            x = self.downsample(x)
+            x = self.downsample(x)           # -> (B, H, W, C)
 
         return x
 
 
 class SwinTransformer(nn.Module):
     """
-    SWIN Transformer
+    SWIN Transformer.
+    (Everything together)
 
     Parameters:
         img_size (int): image size
@@ -433,10 +436,10 @@ class SwinTransformer(nn.Module):
         window_size (int): window size
         mlp_ratio (float): ratio of MLP hidden dim to embedding dim
         qkv_bias (bool, optional): if True, add a learnable bias to query, key, value
-        qk_scale (float | None, optional): override default qk scale of head_dim ** -0.5 if set
-        mlp_drop (float, optional): MLP dropout rate. Default: 0.0
-        attn_drop (float, optional): attention dropout rate. Default: 0.0
-        drop_path (float | tuple[float], optional): stochastic depth rate. Default: 0.0
+        qk_scale (float, optional): override default qk scale of head_dim ** -0.5 if set
+        mlp_drop (float, optional): MLP dropout rate
+        attn_drop (float, optional): attention dropout rate
+        drop_path (float | Tuple[float], optional): stochastic depth rate
         norm_layer (nn.Module, optional): normalization to apply after feature extraction
         patch_norm (nn.Module, optional): normalization for PatchEmbedding layer
 
@@ -487,18 +490,18 @@ class SwinTransformer(nn.Module):
         self.apply(self._init_weights)
 
     def extract_features(self, x):
-        x = self.patch_embed(x)
+        x = self.patch_embed(x)                    # -> (B, H, W, C)
         for layer in self.layers:
-            x = layer(x)
-        x = self.norm(x)
-        x = self.avgpool(x.permute(0, 3, 1, 2))    # x -> (B, C, H, W)
-        x = x.squeeze()
+            x = layer(x)                           
+        x = self.norm(x)                           # -> (B, H, W, C)
+        x = self.avgpool(x.permute(0, 3, 1, 2))    # -> (B, C, 1, 1)
+        x = x.squeeze()                            # -> (B, C)
 
         return x
 
     def forward(self, x):
-        x = self.extract_features(x)
-        x = self.head(x)
+        x = self.extract_features(x)               # -> (B, C)
+        x = self.head(x)                           # -> (B, head_output_dim)
 
         return x
 
